a18d976a9dfc7d4ce45b9593d8f832f450e9b9c3
Ben Sailor <spailll@dhcp-10-227-115-196.eduroam-corewlcs-15.okstate.edu>
2024-01-24 11:14:48 -0600

    Worked on the code. Added classes for each piece as well as the board, began writing functions for calculating valid moves and for moving the pieces. Work In Progress

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pygame
 
+from pieces import Pawn, Bishop, Knight, Rook, Queen, King, Board
+
 WIDTH, HEIGHT = 480, 480
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Chess")
 
 WHITE = (255, 255, 255)
-FPS = 60
+FPS = 10
 
 BLACK_PAWN = pygame.image.load('Pieces/Chess_pdt60.png')
 WHITE_PAWN = pygame.image.load('Pieces/Chess_plt60.png')
@@ -24,83 +26,50 @@ PIECE_SIZE = 60
 
 SQUARE_SIZE = (60,60)
 BLACK_SQUARE = pygame.Surface(SQUARE_SIZE)
-BLACK_SQUARE.fill((211,211,211))
+BLACK_SQUARE.fill((211,211,211))    # Light Grey
 
-def draw_window(pieces,):
+def draw_window(board):
     WIN.fill(WHITE)
 
     for i in range(8):
         for j in range(8):
-            if (i+j) % 2 == 0:
+            if (i+j) % 2 == 1:
                 WIN.blit(BLACK_SQUARE, (i*60,j*60))
 
     i = 0
-    for piece in pieces:
-        if i < 8:
-            image = WHITE_PAWN
-        elif i < 16:
-            image = BLACK_PAWN
-        elif i < 18:
-            image = WHITE_ROOK
-        elif i < 20:
-            image = BLACK_ROOK
-        elif i < 22:
-            image = WHITE_KNIGHT
-        elif i < 24:
-            image = BLACK_KNIGHT
-        elif i < 26:
-            image = WHITE_BISHOP
-        elif i < 28:
-            image = BLACK_BISHOP
-        elif i < 29:
-            image = WHITE_QUEEN
-        elif i < 30:
-            image = BLACK_QUEEN
-        elif i < 31:
-            image = WHITE_KING
-        else:
-            image = BLACK_KING
-        WIN.blit(image, piece)
-        i += 1
+    for piece in board.white_pieces:
+        piece.draw(WIN)
+    for piece in board.black_pieces:
+        piece.draw(WIN)
     pygame.display.update()
 
+def handle_click(pos, board):
+    x, y = pos
+    x = x // 60 * 60
+    y = y // 60 * 60
+    piece_is_selected = False
+    for piece in board.white_pieces:
+        if piece.selected:
+            piece_is_selected = True
+            selected_piece = piece
+    for piece in board.black_pieces:
+        if piece.selected:
+            piece_is_selected = True
+            selected_piece = piece
+    if piece_is_selected:
+        if board.move(selected_piece,x, y):
+            return
+        else:
+            selected_piece.selected = False
+    for piece in board.white_pieces:
+        if piece.x == x and piece.y == y:
+            piece.selected = True
+    for piece in board.black_pieces:
+        if piece.x == x and piece.y == y:
+            piece.selected = True
+
 def main():
-    pawnW1 = pygame.Rect(0,360, PIECE_SIZE, PIECE_SIZE) 
-    pawnB1 = pygame.Rect(0,60, PIECE_SIZE, PIECE_SIZE)
-    pawnW2 = pygame.Rect(60,360, PIECE_SIZE, PIECE_SIZE) 
-    pawnB2 = pygame.Rect(60,60, PIECE_SIZE, PIECE_SIZE)
-    pawnW3 = pygame.Rect(120,360, PIECE_SIZE, PIECE_SIZE) 
-    pawnB3 = pygame.Rect(120,60, PIECE_SIZE, PIECE_SIZE)
-    pawnW4 = pygame.Rect(180,360, PIECE_SIZE, PIECE_SIZE) 
-    pawnB4 = pygame.Rect(180,60, PIECE_SIZE, PIECE_SIZE)
-    pawnW5 = pygame.Rect(240,360, PIECE_SIZE, PIECE_SIZE) 
-    pawnB5 = pygame.Rect(240,60, PIECE_SIZE, PIECE_SIZE)
-    pawnW6 = pygame.Rect(300,360, PIECE_SIZE, PIECE_SIZE) 
-    pawnB6 = pygame.Rect(300,60, PIECE_SIZE, PIECE_SIZE)
-    pawnW7 = pygame.Rect(360,360, PIECE_SIZE, PIECE_SIZE) 
-    pawnB7 = pygame.Rect(360,60, PIECE_SIZE, PIECE_SIZE)
-    pawnW8 = pygame.Rect(420,360, PIECE_SIZE, PIECE_SIZE) 
-    pawnB8 = pygame.Rect(420,60, PIECE_SIZE, PIECE_SIZE)
-    rookW1 = pygame.Rect(0,420, PIECE_SIZE, PIECE_SIZE)
-    rookB1 = pygame.Rect(0,0, PIECE_SIZE, PIECE_SIZE)
-    rookW2 = pygame.Rect(420,420, PIECE_SIZE, PIECE_SIZE)
-    rookB2 = pygame.Rect(420,0, PIECE_SIZE, PIECE_SIZE)
-    knightW1 = pygame.Rect(60,420, PIECE_SIZE, PIECE_SIZE)
-    knightB1 = pygame.Rect(60,0, PIECE_SIZE, PIECE_SIZE)
-    knightW2 = pygame.Rect(360,420, PIECE_SIZE, PIECE_SIZE)
-    knightB2 = pygame.Rect(360,0, PIECE_SIZE, PIECE_SIZE)
-    bishopW1 = pygame.Rect(120,420, PIECE_SIZE, PIECE_SIZE)
-    bishopB1 = pygame.Rect(120,0, PIECE_SIZE, PIECE_SIZE)
-    bishopW2 = pygame.Rect(300,420, PIECE_SIZE, PIECE_SIZE)
-    bishopB2 = pygame.Rect(300,0, PIECE_SIZE, PIECE_SIZE)
-    queenW = pygame.Rect(180,420, PIECE_SIZE, PIECE_SIZE)
-    queenB = pygame.Rect(180,0, PIECE_SIZE, PIECE_SIZE)
-    kingW = pygame.Rect(240,420, PIECE_SIZE, PIECE_SIZE)
-    kingB = pygame.Rect(240,0, PIECE_SIZE, PIECE_SIZE)
-
-
-    pieces = [pawnW1, pawnW2, pawnW3, pawnW4, pawnW5, pawnW6, pawnW7, pawnW8, pawnB1, pawnB2, pawnB3, pawnB4, pawnB5, pawnB6, pawnB7, pawnB8, rookW1, rookW2, rookB1, rookB2, knightW1, knightW2, knightB1, knightB2, bishopW1, bishopW2, bishopB1, bishopB2, queenW, queenB, kingW, kingB]
-
+    board = Board()
     run = True
     clock = pygame.time.Clock()
 
@@ -109,8 +78,11 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-
-        draw_window(pieces)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = event.pos
+                handle_click(mouse_pos, board)                
+               
+        draw_window(board)
 
     pygame.quit()
 
---
+++ pieces.py
@@ -0,0 +1,328 @@
+import pygame
+
+PIECE_SIZE = 60
+
+class Board:
+    def __init__(self):
+        # initialize and add all pieces to board
+        self.white_pieces = [Pawn(0,360,'white'), Pawn(60,360,'white'), 
+                             Pawn(120,360,'white'), Pawn(180,360,'white'), 
+                             Pawn(240,360,'white'), Pawn(300,360,'white'), 
+                             Pawn(360,360,'white'), Pawn(420,360,'white'), 
+                             Rook(0,420,'white'), Rook(420,420,'white'), 
+                             Knight(60,420,'white'), Knight(360,420,'white'), 
+                             Bishop(120,420,'white'), Bishop(300,420,'white'), 
+                             Queen(180,420,'white'), King(240,420,'white')]
+
+
+
+        self.black_pieces = [Pawn(0,60,'black'), Pawn(60,60,'black'), 
+                             Pawn(120,60,'black'), Pawn(180,60,'black'), 
+                             Pawn(240,60,'black'), Pawn(300,60,'black'), 
+                             Pawn(360,60,'black'), Pawn(420,60,'black'), 
+                             Rook(0,0,'black'), Rook(420,0,'black'), 
+                             Knight(60,0,'black'), Knight(360,0,'black'), 
+                             Bishop(120,0,'black'), Bishop(300,0,'black'), 
+                             Queen(180,0,'black'), King(240,0,'black')]
+
+    def move(self, piece, x, y):
+        for piece2 in self.white_pieces:
+            piece2.find_unfiltered_moves()
+        for piece2 in self.black_pieces:
+            piece2.find_unfiltered_moves()
+        for piece2 in self.white_pieces:
+            self.find_allowed_moves(piece2)
+        for piece2 in self.black_pieces:
+            self.find_allowed_moves(piece2)
+        for move in piece.allowed_moves:
+            if x == move[0] and y == move[1]:
+                piece.x = x
+                piece.y = y
+                piece.rect = pygame.Rect(piece.x, piece.y, PIECE_SIZE, PIECE_SIZE)
+                piece.has_moved = True
+                piece.selected = False
+                if piece.color == 'black':
+                    for piece2 in self.white_pieces:
+                        if piece2.x == x and piece2.y == y:
+                            self.white_pieces.remove(piece2)
+                else:
+                    for piece2 in self.black_pieces:
+                        if piece2.x == x and piece2.y == y:
+                            self.black_pieces.remove(piece2)
+
+
+    def find_allowed_moves(self, piece):
+        piece.allowed_moves = piece.unfiltered_moves
+        for move in piece.allowed_moves:
+            # check if one of our pieces is in that spot
+            if piece.color == 'white':
+                for piece2 in self.white_pieces:
+                    if move == (piece2.x, piece2.y):
+                        piece.allowed_moves.remove(move) 
+            else:
+                for piece2 in self.black_pieces:
+                    if move == (piece2.x, piece2.y):
+                        piece.allowed_moves.remove(move)
+            # specific piece checks
+        for move in piece.allowed_moves:
+            if piece.type == 'pawn':
+                if piece.color == 'white':
+                    if move[0] == piece2.x + 60 or move[0] == piece2.x - 60:
+                        tag = False
+                        for piece2 in self.black_pieces:
+                            if piece2.x == move[0] and piece2.y == move[1]:
+                                tag = True
+                        if tag == False:
+                            piece.allowed_moves.remove(move)
+                else:
+                    if move[0] == piece2.x + 60 or move[0] == piece2.x - 60:
+                        tag = False
+                        for piece2 in self.white_pieces:
+                            if piece2.x == move[0] and piece2.y == move[1]:
+                                tag = True
+                        if tag == False:
+                            piece.allowed_moves.remove(move)
+
+            ##if piece.type == 'bishop' or piece.type == 'queen':
+
+            # if piece.type == 'rook' or piece.type == 'queen' or piece.type == 'pawn':
+            #     if move[1] > piece.y + 60:
+            #         for i in range(piece.y, move[1], 60):
+            #             for piece2 in self.white_pieces:
+            #                 if piece2.x == move[0] and piece2.y == i:
+            #                     piece.allowed_moves.remove(move)
+            #             for piece2 in self.black_pieces:
+            #                 if piece2.x == move[0] and piece2.y == i:
+            #                     piece.allowed_moves.remove(move)
+        
+
+
+
+class Pawn:
+    def __init__(self, x, y, color):
+        self.x = x
+        self.y = y
+        self.color = color
+        self.type = 'pawn'
+        if self.color == 'white':
+            self.image = pygame.image.load('Pieces/Chess_plt60.png')
+        else:
+            self.image = pygame.image.load('Pieces/Chess_pdt60.png')
+        self.rect = pygame.Rect(self.x, self.y, PIECE_SIZE, PIECE_SIZE)
+        self.selected = False
+        self.has_moved = False
+        self.allowed_moves = []
+        self.unfiltered_moves = []
+        self.find_unfiltered_moves()
+
+    def find_unfiltered_moves(self):
+        self.unfiltered_moves = []
+        if self.color == 'white':
+            if self.has_moved == False:
+                self.unfiltered_moves.append((self.x, self.y-120))
+            self.unfiltered_moves.append((self.x+60, self.y-60))
+            self.unfiltered_moves.append((self.x, self.y-60))
+            self.unfiltered_moves.append((self.x-60, self.y-60))
+        else:
+            if self.has_moved == False:
+                self.unfiltered_moves.append((self.x, self.y+120))
+            self.unfiltered_moves.append((self.x+60, self.y+60))
+            self.unfiltered_moves.append((self.x, self.y+60))
+            self.unfiltered_moves.append((self.x-60, self.y+60))
+
+    def draw(self, win):
+        win.blit(self.image, self.rect)
+
+
+
+class Bishop:
+    def __init__(self, x, y, color):
+        self.x = x
+        self.y = y
+        self.color = color
+        self.type = 'bishop'
+        if self.color == 'white':
+            self.image = pygame.image.load('Pieces/Chess_blt60.png')
+        else:
+            self.image = pygame.image.load('Pieces/Chess_bdt60.png')
+        self.rect = pygame.Rect(self.x, self.y, PIECE_SIZE, PIECE_SIZE)
+        self.selected = False
+        self.allowed_moves = []
+        self.unfiltered_moves = []
+        self.find_unfiltered_moves()  
+
+    def find_unfiltered_moves(self):
+        self.unfiltered_moves = []
+        for i in range(1,8):
+            if self.x + i*60 <= 420 and self.y + i*60 <= 420:
+                self.unfiltered_moves.append((self.x+i*60, self.y+i*60))
+            if self.x - i*60 >= 0 and self.y + i*60 <= 420:
+                self.unfiltered_moves.append((self.x-i*60, self.y+i*60))
+            if self.x + i*60 <= 420 and self.y - i*60 >= 0:
+                self.unfiltered_moves.append((self.x+i*60, self.y-i*60))
+            if self.x - i*60 >= 0 and self.y - i*60 >= 0:
+                self.unfiltered_moves.append((self.x-i*60, self.y-i*60))        
+
+    def draw(self, win):
+        win.blit(self.image, self.rect)
+
+
+
+class Knight:
+    def __init__(self, x, y, color):
+        self.x = x
+        self.y = y
+        self.color = color
+        self.type = 'knight'
+        if self.color == 'white':
+            self.image = pygame.image.load('Pieces/Chess_nlt60.png')
+        else:
+            self.image = pygame.image.load('Pieces/Chess_ndt60.png')
+        self.rect = pygame.Rect(self.x, self.y, PIECE_SIZE, PIECE_SIZE)
+        self.selected = False
+        self.allowed_moves = []
+        self.unfiltered_moves = []
+        self.find_unfiltered_moves()
+
+    def find_unfiltered_moves(self):
+        self.unfiltered_moves = []
+        for i in range(4):
+            if i == 0 and self.x - 120 >= 0 and self.x - 120 <= 420:
+                if self.y - 60 >= 0 and self.y - 60 <= 420:
+                    self.unfiltered_moves.append((self.x-120, self.y-60))
+                if self.y + 60 >= 0 and self.y + 60 <= 420:
+                    self.unfiltered_moves.append((self.x-120, self.y+60))
+            elif i == 1 and self.x - 60 >= 0 and self.x - 60 <= 420:
+                if self.y - 120 >= 0 and self.y - 120 <= 420:
+                    self.unfiltered_moves.append((self.x-60, self.y-120))
+                if self.y + 120 >= 0 and self.y + 120 <= 420:
+                    self.unfiltered_moves.append((self.x-60, self.y+120))
+            elif i == 2 and self.x + 60 >= 0 and self.x + 60 <= 420:
+                if self.y - 120 >= 0 and self.y - 120 <= 420:
+                    self.unfiltered_moves.append((self.x+60, self.y-120))
+                if self.y + 120 >= 0 and self.y + 120 <= 420:
+                    self.unfiltered_moves.append((self.x+60, self.y+120))
+            elif i == 3 and self.x + 120 >= 0 and self.x + 120 <= 420:
+                if self.y - 60 >= 0 and self.y - 60 <= 420:
+                    self.unfiltered_moves.append((self.x+120, self.y-60))
+                if self.y + 60 >= 0 and self.y + 60 <= 420:
+                    self.unfiltered_moves.append((self.x+120, self.y+60))
+
+    def draw(self, win):
+        win.blit(self.image, self.rect)
+
+
+
+class Rook:
+    def __init__(self, x, y, color):
+        self.x = x
+        self.y = y
+        self.color = color
+        self.type = 'rook'
+        if self.color == 'white':
+            self.image = pygame.image.load('Pieces/Chess_rlt60.png')
+        else:
+            self.image = pygame.image.load('Pieces/Chess_rdt60.png')
+        self.rect = pygame.Rect(self.x, self.y, PIECE_SIZE, PIECE_SIZE)
+        self.selected = False
+        self.has_moved = False
+        self.allowed_moves = []
+        self.unfiltered_moves = []
+        self.find_unfiltered_moves()
+
+    def find_unfiltered_moves(self):
+        self.unfiltered_moves = []
+        for i in range(1,8):
+            if self.x + i*60 <= 420:
+                self.unfiltered_moves.append((self.x+i*60, self.y))
+            if self.x - i*60 >= 0:
+                self.unfiltered_moves.append((self.x-i*60, self.y))
+            if self.y + i*60 <= 420:
+                self.unfiltered_moves.append((self.x, self.y+i*60))
+            if self.y - i*60 >= 0:
+                self.unfiltered_moves.append((self.x, self.y-i*60))
+
+
+    def draw(self, win):
+        win.blit(self.image, self.rect)
+
+
+
+class Queen:
+    def __init__(self, x, y, color):
+        self.x = x
+        self.y = y
+        self.color = color
+        self.type = 'queen'
+        if self.color == 'white':
+            self.image = pygame.image.load('Pieces/Chess_qlt60.png')
+        else:
+            self.image = pygame.image.load('Pieces/Chess_qdt60.png')
+        self.rect = pygame.Rect(self.x, self.y, PIECE_SIZE, PIECE_SIZE)
+        self.selected = False
+        self.allowed_moves = []
+        self.unfiltered_moves = []
+        self.find_unfiltered_moves()
+
+    def find_unfiltered_moves(self):
+        self.unfiltered_moves = []
+        for i in range(1,8):
+            if self.x + i*60 <= 420 and self.y + i*60 <= 420:
+                self.unfiltered_moves.append((self.x+i*60, self.y+i*60))
+            if self.x - i*60 >= 0 and self.y + i*60 <= 420:
+                self.unfiltered_moves.append((self.x-i*60, self.y+i*60))
+            if self.x + i*60 <= 420 and self.y - i*60 >= 0:
+                self.unfiltered_moves.append((self.x+i*60, self.y-i*60))
+            if self.x - i*60 >= 0 and self.y - i*60 >= 0:
+                self.unfiltered_moves.append((self.x-i*60, self.y-i*60))
+            if self.x + i*60 <= 420:
+                self.unfiltered_moves.append((self.x+i*60, self.y))
+            if self.x - i*60 >= 0:
+                self.unfiltered_moves.append((self.x-i*60, self.y))
+            if self.y + i*60 <= 420:
+                self.unfiltered_moves.append((self.x, self.y+i*60))
+            if self.y - i*60 >= 0:
+                self.unfiltered_moves.append((self.x, self.y-i*60))
+
+    def draw(self, win):
+        win.blit(self.image, self.rect)
+
+
+
+class King:
+    def __init__(self, x, y, color):
+        self.x = x
+        self.y = y
+        self.color = color
+        self.type = 'king'
+        if self.color == 'white':
+            self.image = pygame.image.load('Pieces/Chess_klt60.png')
+        else:
+            self.image = pygame.image.load('Pieces/Chess_kdt60.png')
+        self.rect = pygame.Rect(self.x, self.y, PIECE_SIZE, PIECE_SIZE)
+        self.selected = False
+        self.has_moved = False
+        self.allowed_moves = []
+        self.unfiltered_moves = []
+        self.find_unfiltered_moves()
+
+    def find_unfiltered_moves(self):
+        if self.x + 60 <= 420 and self.y + 60 <= 420:
+            self.unfiltered_moves.append((self.x+60, self.y+60))
+        if self.x - 60 >= 0 and self.y + 60 <= 420:
+            self.unfiltered_moves.append((self.x-60, self.y+60))
+        if self.x + 60 <= 420 and self.y - 60 >= 0:
+            self.unfiltered_moves.append((self.x+60, self.y-60))
+        if self.x - 60 >= 0 and self.y - 60 >= 0:
+            self.unfiltered_moves.append((self.x-60, self.y-60))
+        if self.x + 60 <= 420:
+            self.unfiltered_moves.append((self.x+60, self.y))
+        if self.x - 60 >= 0:
+            self.unfiltered_moves.append((self.x-60, self.y))
+        if self.y + 60 <= 420:
+            self.unfiltered_moves.append((self.x, self.y+60))
+        if self.y - 60 >= 0:
+            self.unfiltered_moves.append((self.x, self.y-60))
+
+    def draw(self, win):
+        win.blit(self.image, self.rect)
